8bf1d7e7a9770411ca58201cb07aad35e20b1798
= <=>
2019-06-10 12:47:50 -0300

    fim do post

--- formRecaptcha/src/Controller/FormController.php
+++ formRecaptcha/src/Controller/FormController.php
@@ -1,5 +1,8 @@
 <?php
+namespace App\Controller;
 
+use ReCaptcha\ReCaptcha;
+use Swift_Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,16 +20,35 @@ class FormController extends AbstractController
     /**
      * @Route("/envia",methods={"POST"})
      * @param Request $request
+     * @param Swift_Mailer $mailer
+     * @param ReCaptcha $reCaptcha
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
      */
-    public function index(Request $request)
+    public function envia(Request $request,Swift_Mailer $mailer,ReCaptcha $reCaptcha)
     {
         $nome = $request->get("nome");
         $assunto = $request->get("assunto");
         $email = $request->get("email");
         $conteudo = $request->get("conteudo");
+        $recaptchaToken = $request->get("recaptchaToken");
 
+        $resposta = $reCaptcha->verify($recaptchaToken);
+        $naoEhUmBot = $resposta->isSuccess();
 
+        if($naoEhUmBot) {
+            $message = (new \Swift_Message())
+                ->setSubject($assunto)
+                ->setFrom($email)
+                ->setTo("contato@hefesto.io")
+                ->setBody("E-mail de contato do {$nome}:\n\n{$conteudo}",
+                    'text/html'
+                );
+
+            $mailer->send($message);
+        } else {
+            throw new \Exception("Token inválido");
+        }
 
         return $this->render("index.html.twig");
     }
